0fa50fc298afe257ad2fa9b96cae3f75c93aeb52
weijj0528 <weijj0528@163.com>
2018-05-15 11:31:54 +0800

    fullcalendar引入安装

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 import 'viewerjs/dist/viewer.min.css';
+import 'fullcalendar/dist/fullcalendar.min.css';
 
 import router from './router'
 import app from './views/app.vue'
