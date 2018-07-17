53eea1ed4d7bb53cd973ce493160b775fc3ac384
weijj0528 <weijj0528@163.com>
2018-07-17 09:28:07 +0800

    调整

--- src/template/template.vue
+++ src/template/template.vue
@@ -1,30 +1,33 @@
 <!-- Created by Weiun on 2017/1/17.-->
 <!-- 模板组件 -->
 <template>
-    <div>组件模板</div>
+  <div>组件模板</div>
 </template>
 <script>
 import { mapActions, mapGetters, mapMutations } from "vuex";
 export default {
-  data() {
-    return {};
-  },
-  computed: {
-    ...mapGetters([])
-  },
-  mounted: function() {
-    this.$nextTick(function() {
-      // 代码保证 this.$el 在 document 中
-      //                oc
-    });
-  },
-  beforeDestroy() {},
-  methods: {
-    ...mapActions([]),
-    hellow() {
-      console.log("hellow world");
-    }
-  }
+    components: {},
+    props: {},
+    data() {
+        return {};
+    },
+    computed: {
+        ...mapGetters([])
+    },
+    mounted: function() {
+        this.$nextTick(function() {
+            // 代码保证 this.$el 在 document 中
+            //                oc
+        });
+    },
+    beforeDestroy() {},
+    methods: {
+        ...mapActions([]),
+        hellow() {
+            console.log("hellow world");
+        }
+    },
+    watch: {}
 };
 </script>
 <style scoped>
